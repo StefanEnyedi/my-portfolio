aa8471bf66d719c6f1704e1914db91c68db9227b
Stefan Enyedi <97853134+StefanEnyedi@users.noreply.github.com>
2022-08-16 23:50:23 +0300

    perf: optimize navigation bar on mobile

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
 function myFunction(){
     var x = document.getElementById("MainLinks");
-    if (x.style.display === 'none') {
-        x.style.display = 'block';
-    } else {
-        x.style.display = 'none';
-    }
+    x.classList.toggle("hidden");
 }
